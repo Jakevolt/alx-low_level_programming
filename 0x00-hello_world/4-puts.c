e0686db2ffc3a6f0d424d7c36d808f07ff6171d3
Jakevolt <alokujacob@gmail.com>
2023-02-16 13:42:17 -0800

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@ gcc $CFILE -o cisfun
 root@dc6ab1e9ea70:/alx-low_level_programming/0x00-hello_world# cat 4-puts.c
 #include <stdio.h>
 /
-*main - Entry point
+*main -Entry point
 *Return: Always 0 (success)
-*
-	* /
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-                return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
 }
