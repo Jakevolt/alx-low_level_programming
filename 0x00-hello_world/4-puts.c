67d00a177afb56ab95807fd682126572a6441e3b
Jakevolt <alokujacob@gmail.com>
2023-02-16 14:00:38 -0800

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
-root@dc6ab1e9ea70:/alx-low_level_programming/0x00-hello_world# cat 4-puts.c
 #include <stdio.h>
-/
-*main -Entry point
-*Return: Always 0 (success)
-*/
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
